e2ca188dfcb3b032f88fd3ab87e9fc24775ee2d1
RaheemaMuhammed <121420610+RaheemaMuhammed@users.noreply.github.com>
2023-11-03 10:28:11 +0530

    backend fixed

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,3 +1,55 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser,BaseUserManager,PermissionsMixin
+import uuid
+from django.utils import timezone
+import json
 # Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    """Define a model manager for User model with no username field."""
+
+    def _create_user(self, email,username, password=None, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email,username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email,username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email,username,password, **extra_fields)
+
+    def create_superuser(self, email,username, password=None, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True )
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email,username,password, **extra_fields)
+        
+
+
+class CustomUser(AbstractUser,PermissionsMixin):
+    email=models.EmailField(max_length=100, unique=True)
+    username=models.CharField(max_length=250,null=True)
+    profile_pic=models.ImageField(null=True,upload_to='userprofile/')
+    is_active=models.BooleanField(default=False)
+    is_user=models.BooleanField(default=False)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = CustomUserManager()
+
+
+
+
