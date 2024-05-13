c0a23eb0c704071b82c14a03971d6b6125f4d160
RaheemaMuhammed <121420610+RaheemaMuhammed@users.noreply.github.com>
2024-05-13 18:06:26 +0530

    asgi.py file changed

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -8,7 +8,6 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 from django.core.asgi import get_asgi_application
-django_asgi_app = get_asgi_application()
 import os
 
 from channels.routing import ProtocolTypeRouter,URLRouter
@@ -17,6 +16,7 @@ from chat.routing import websocket_urlpatterns
 from chat.token_auth import JwtAuthMiddlewareStack
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
