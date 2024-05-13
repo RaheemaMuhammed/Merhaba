88300f837219c7e9eb098829fbb4113ad72e01d3
RaheemaMuhammed <121420610+RaheemaMuhammed@users.noreply.github.com>
2024-05-13 18:12:33 +0530

    asgi.py file changed

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -9,12 +9,13 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 from django.core.asgi import get_asgi_application
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
 
 from channels.routing import ProtocolTypeRouter,URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from chat.routing import websocket_urlpatterns
 from chat.token_auth import JwtAuthMiddlewareStack
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 django_asgi_app = get_asgi_application()
 
