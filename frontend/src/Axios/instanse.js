ccba58a948319de3dfaeaf648d7f390057809f0b
RaheemaMuhammed <121420610+RaheemaMuhammed@users.noreply.github.com>
2024-05-14 17:17:39 +0530

    axios instance updated

--- frontend/src/Axios/instanse.js
+++ frontend/src/Axios/instanse.js
@@ -1,3 +1,7 @@
 import axios from 'axios'
 
-export const axiosInstance=axios.create({baseURL:'http://127.0.0.1:8000/'})
+export const baseURLUser = import.meta.env.VITE_DEVELOP === 'True'
+  ? 'http://127.0.0.1:8000/'
+  : 'https://backendapi.site/';
+
+export const axiosInstance=axios.create({baseURL:baseURLUser})
